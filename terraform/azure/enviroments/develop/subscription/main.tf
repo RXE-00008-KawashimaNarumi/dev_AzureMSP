41794389e8ff56a8e48e2dfd3ae3fe3f54880608
RXE-00008-KawashimaNarumi <rxe00008@partner.nri.co.jp>
2024-02-08 15:45:32 +0900

    modify

--- terraform/azure/enviroments/develop/subscription/main.tf
+++ terraform/azure/enviroments/develop/subscription/main.tf
@@ -5,3 +5,11 @@ module "create_suport_acount" {
   password = "${var.password}"
 }
 
+module "user_invitation" {
+  source = "../../../modules/user"
+  user_display_name = "${var.user_display_name}"
+  user_email_address = "${var.user_email_address}"
+  redirect_url = "${var.redirect_url}"
+  user_type = "${var.user_type}"
+}
+
