a29555c149b0c375de9288dcec8dea8889398a89
RXE-00008-KawashimaNarumi <rxe00008@partner.nri.co.jp>
2024-02-08 15:52:12 +0900

    modify

--- terraform/azure/enviroments/develop/subscription/variable.tf
+++ terraform/azure/enviroments/develop/subscription/variable.tf
@@ -1,9 +1,6 @@
-#create user
 variable "user_principal_name" {}
 variable "display_name" {}
 variable "password" {}
-
-# invitation user
 variable "user_display_name" {}
 variable "user_email_address" {}
 variable "redirect_url" {}
