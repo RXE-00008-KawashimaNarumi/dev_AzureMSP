8b15b482022582d654c0c02d8467c0fcd0f8316b
RXE-00008-KawashimaNarumi <rxe00008@partner.nri.co.jp>
2024-02-21 03:21:17 +0900

    modify

--- terraform/azure/enviroments/develop/subscription/main.tf
+++ terraform/azure/enviroments/develop/subscription/main.tf
@@ -15,9 +15,9 @@ module "user_invitation" {
 }
 
 
-module "role_assignment" {
-  source = "../../../modules/role_assignment"
-  subscription_id = "${var.subscription_id}"
-  role_user_principal_name = "${var.role_user_principal_name}"
-  role_definition_name = "${var.role_definition_name}"
-}
+#module "role_assignment" {
+#  source = "../../../modules/role_assignment"
+#  subscription_id = "${var.subscription_id}"
+#  role_user_principal_name = "${var.role_user_principal_name}"
+#  role_definition_name = "${var.role_definition_name}"
+#}
