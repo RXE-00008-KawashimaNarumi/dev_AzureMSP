92bb03556431eeec6a1bfda97542c5ec7c416934
RXE-00008-KawashimaNarumi <rxe00008@partner.nri.co.jp>
2024-02-21 03:09:06 +0900

    modify

--- terraform/azure/enviroments/develop/subscription/main.tf
+++ terraform/azure/enviroments/develop/subscription/main.tf
@@ -1,18 +1,18 @@
-#module "create_supprt_acount" {
-#  source = "../../../modules/user"
-#  user_principal_name = "${var.user_principal_name}"
-#  display_name = "${var.display_name}"
-#  password = "${var.password}"
-#}
+module "create_supprt_acount" {
+  source = "../../../modules/user"
+  user_principal_name = "${var.user_principal_name}"
+  display_name = "${var.display_name}"
+  password = "${var.password}"
+}
 
-#module "user_invitation" {
-#  source = "../../../modules/user_invitation"
-#  user_display_name = "${var.user_display_name}"
-#  user_email_address = "${var.user_email_address}"
-#  redirect_url = "${var.redirect_url}"
-#  user_type = "${var.user_type}"
-#  additional_recipients = "${var.additional_recipients}"
-#}
+module "user_invitation" {
+  source = "../../../modules/user_invitation"
+  user_display_name = "${var.user_display_name}"
+  user_email_address = "${var.user_email_address}"
+  redirect_url = "${var.redirect_url}"
+  user_type = "${var.user_type}"
+  additional_recipients = "${var.additional_recipients}"
+}
 
 
 module "role_assignment" {
