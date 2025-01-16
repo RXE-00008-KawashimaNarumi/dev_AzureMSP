044eae6c7c2ad78aff66f498c2901890c66fc6dd
RXE-00008-KawashimaNarumi <152465594+RXE-00008-KawashimaNarumi@users.noreply.github.com>
2025-01-17 07:33:48 +0900

    add

--- terraform/azure/enviroments/test/945_create_virtual_machine/main.tf
+++ terraform/azure/enviroments/test/945_create_virtual_machine/main.tf
@@ -1,4 +1,4 @@
 module "create_virtual_machine" {
-  source                  = "../../../modules/945_create_virtual_machine"
+  source                  = "../../modules/945_create_virtual_machine"
   admin_password         = var.admin_password
 }
